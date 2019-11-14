176e6e8fefc6e8433021f2023eba9ee0c383b3a8
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2019-11-14 01:09:05 +0100

    improve uix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     message="This email is already use"
  * )
  */
-class User implements  UserInterface
+class User implements  UserInterface, \serializable
 {
     /**
      * @ORM\Id()
@@ -148,5 +148,30 @@ class User implements  UserInterface
     public function eraseCredentials()
     {
         // TODO: Implement eraseCredentials() method.
+    }
+
+    /**
+     * String representation of object
+     * @link https://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+
+    }
+
+    /**
+     * Constructs the object
+     * @link https://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        // TODO: Implement unserialize() method.
     }
 }
